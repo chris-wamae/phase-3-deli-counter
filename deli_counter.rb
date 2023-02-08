e0f58ae875e46c74e414b74d569a8207a1a1ed38
chris-wamae <chriswamae123@gmail.com>
2023-02-08 06:26:57 +0300

    Completed Assignment

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,39 @@
 # Write your code here.
+require "pry"
+
+katz_deli = []
+
+def line katz_deli
+    if
+    katz_deli.empty? 
+    puts "The line is currently empty."
+    "The line is currently empty."
+    else
+    numbered = katz_deli.map do |name|
+    "#{katz_deli.index(name) + 1}. #{name}"
+    "#{katz_deli.index(name) + 1}. #{name}"
+     end
+    puts "The line is currently: " + numbered.join(" ")
+    end
+    
+end
+
+def take_a_number katz_deli ,name
+puts "Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
+"Welcome, #{name}. You are number #{katz_deli.length + 1} in line."
+if
+katz_deli.empty?
+  katz_deli.push(name)
+else
+   katz_deli.push(name)
+end
+end
+
+def now_serving katz_deli
+    if katz_deli.empty?
+        puts "There is nobody waiting to be served!"
+    else
+    puts "Currently serving #{katz_deli[0]}."
+    katz_deli.shift
+    end
+end
